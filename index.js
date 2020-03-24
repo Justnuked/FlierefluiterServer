ef6bb280cff353bb3ca82a651ae9781128208659
KRademacher <Kevin_Rademacher@hotmail.com>
2020-03-24 23:42:16 +0100

    Enabling CORS for Angular front-end

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ require('./src/config/passport.js');
 
 app.use('*', function (req, res, next) {
 	res.contentType('application/json');
+	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
