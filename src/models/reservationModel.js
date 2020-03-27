54889ff367721fb925bdf5498a357d9ae5504796
KRademacher <Kevin_Rademacher@hotmail.com>
2020-03-27 17:10:08 +0100

    Fixed new reservation property to our convention

--- src/models/reservationModel.js
+++ src/models/reservationModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ReservationSchema = new Schema({
-    startDate: {
+    startdate: {
         type: Date,
         required: true
     },
