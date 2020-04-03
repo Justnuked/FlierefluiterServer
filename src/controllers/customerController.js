91366f34902a9bd979298265ab75ff4c0c2a4497
KRademacher <Kevin_Rademacher@hotmail.com>
2020-04-03 12:28:16 +0200

    Merge dev into feature_hateoas

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -10,13 +10,11 @@ module.exports = {
 
         Customer.findOne({ idnumber: req.body.idnumber }, function (err, data) {
             //If server error
-            if (err)
-            {
+            if (err) {
                 res.status(500).send(err);
             }
             //Customer doesn't exist yet.
-            if (data === null)
-            {
+            if (data === null) {
                 let customer = new Customer({
                     name: req.body.name,
                     address: req.body.address,
@@ -36,8 +34,7 @@ module.exports = {
                             Customer: result
                         });
                     }).catch(next);
-            } else
-            {
+            } else {
                 res.status(400).send({ Error: 'Customer already exists.' });
             }
         }).catch(next);
@@ -46,11 +43,9 @@ module.exports = {
     //Get all customers
     get(req, res, next) {
 
-
         Customer.find({}, function (err, data) {
             //If server error
-            if (err)
-            {
+            if (err) {
                 res.status(500).send(err);
             }
             //Check fetched data
@@ -78,20 +73,15 @@ module.exports = {
 
     getCustomerThatIsLoggedIn(req, res, next) {
         const username = req.user.username;
-        console.log(req.user.username);
-        const role = req.user.role;
 
-        if (utils.checkIsInRole(req.user, ROLES.Manager, ROLES.Reception, ROLES.Admin, ROLES.GroundsKeeper, ROLES.Customer))
-        {
+        if (utils.checkIsInRole(req.user, ROLES.Manager, ROLES.Reception, ROLES.Admin, ROLES.GroundsKeeper, ROLES.Customer)) {
             Customer.findOne({ username: username }, function (err, data) {
                 //If server error
-                if (err)
-                {
+                if (err) {
                     res.status(500).send(err);
                 }
                 //Return fetched data
-                else
-                {
+                else {
                     res.status(200).send(data);
                 }
             }).catch(next);
@@ -102,8 +92,7 @@ module.exports = {
     getById(req, res, next) {
         Customer.findById({ _id: req.params.id }, function (err, data) {
             //If server error
-            if (err)
-            {
+            if (err) {
                 res.status(500).send(err);
             }
             //Check fetched data
@@ -150,13 +139,11 @@ module.exports = {
             })
             .then((customer) => {
                 //Check if retrieved customer exists.
-                if (customer === null)
-                {
+                if (customer === null) {
                     res.status(400).send({ Error: 'Customer does not exist.' });
                 }
                 //Else confirm update and show new data
-                else
-                {
+                else {
                     res.status(200).send({
                         Message: 'Customer edited successfully.',
                         Customer: customer
@@ -170,11 +157,9 @@ module.exports = {
 
         Customer.findById({ _id: req.params.id })
             .then((customer) => {
-                if (customer === null)
-                {
+                if (customer === null) {
                     res.status(400).send({ Error: 'Customer does not exist.' });
-                } else
-                {
+                } else {
                     customer.delete()
                         .then(() => {
                             res.status(200).send({ Message: 'Customer has been removed successfully.' });
